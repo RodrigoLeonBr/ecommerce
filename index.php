f5adf570c5240a1be7d316e588f1bf5303f559ae
Rodrigo Alexander Diaz Leon <rodrigo_leonbr@yahoo.com.br>
2018-06-30 16:42:24 -0300

    site menu categoria

--- index.php
+++ index.php
@@ -314,6 +314,22 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category;
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'produtcs'=>[]
+	]);
+	
+
+});
+
 
 $app->run();
 
