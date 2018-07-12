01f400e330f8b19d227aacff845d568faa2bda5f
Rodrigo Alexander Diaz Leon <rodrigo_leonbr@yahoo.com.br>
2018-07-11 23:02:53 -0300

    Meus Pedidos e carrinho total

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php 
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 function formatPrice($vlprice){
 
@@ -25,6 +26,29 @@ function getUserName()
 	return $user->getdesperson();
 
 }
+
+function getCartNrQtd()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+
+}
+
+function getCartVlSubtotal()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
+
+}
+
 
 function post($key)
 {
