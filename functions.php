bf942f68823134c6760218c1ebd3faf2bd8eee3f
Rodrigo Alexander Diaz Leon <rodrigo_leonbr@yahoo.com.br>
2018-07-14 22:42:17 -0300

    Acompanhar status do pedido

--- functions.php
+++ functions.php
@@ -3,11 +3,19 @@
 use \Hcode\Model\User;
 use \Hcode\Model\Cart;
 
-function formatPrice($vlprice){
+function formatPrice($vlprice)
+{
 
 	if (!$vlprice > 0) $vlprice = 0;
 
 	return number_format($vlprice, 2, ",", ".");
+
+}
+
+function formatDate($date)
+{
+
+	return date('d/m/Y', strtotime($date));
 
 }
 
